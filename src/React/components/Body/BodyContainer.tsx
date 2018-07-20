a103c14e4835d53b785e09d526a0891db692f551
Adanna Omatu <ao60@FCMCO-C07P43EVG1HW.FCM.ADMIN.NYU.EDU>
2018-07-20 11:01:13 -0400

    left header styles complete

--- src/React/components/Body/BodyContainer.tsx
+++ src/React/components/Body/BodyContainer.tsx
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import LeftHeader from './LeftPanel/LeftPanelHeader';
+// import RightHeader from './RightPanel/RightPanelHeader';
+// import LeftBody from './LeftPanel/LeftPanelBody/Container';
+// import RightBody from './RightPanel/RightPanelBody/Container';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 type Props = {};
 export default class Body extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>o</Text>
+        <View style={styles.header}>
+          <LeftHeader />
+
+          <View style={styles.rightPanel} />
+        </View>
+        <View style={styles.body} />
       </View>
     );
   }
@@ -17,20 +25,21 @@ export default class Body extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 8,
-    justifyContent: 'center',
-    alignItems: 'center',
     borderWidth: 1,
     borderStyle: 'solid',
-    borderColor: '#BDBDBD'
+    borderColor: '#BDBDBD',
+    // backgroundColor: 'green',
+    paddingTop: '1%',
+    paddingLeft: '3%',
+    paddingRight: '3%'
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10
+  header: {
+    flex: 0.3,
+    flexDirection: 'row'
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5
+  rightPanel: { flex: 1, backgroundColor: 'yellow' },
+  body: {
+    flex: 5,
+    backgroundColor: 'red'
   }
 });
